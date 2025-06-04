a35f79ca21db6b063922cac6b015a94789292e96
Çağdaş Kılıç <kilic.cagdas@metu.edu.tr>
2025-06-04 17:34:32 -0400

    N>T case is fixed

--- marchenko_pastur.py
+++ marchenko_pastur.py
@@ -6,63 +6,56 @@ from scipy.signal import unit_impulse
 '''
 parameters
 '''
-# number of states
-N = 1000
+# number of variables
+N = 6000
 # number of observations
-T = 2000
+T = 3000
 # ratio of num. of states to num. of observations
 q = N/T
 
 # non-symmetric random matrix
 X = np.random.normal(0, 1, (T, N))
 # NxN covariance
-Y = 1/T * X @ X.T
+Y = 1/T * X.T @ X
 # compute the eigenvalues
 eigval = np.linalg.eigvalsh(Y)
 
-# marcenko-pastur bounds
+# Marcenko-Pastur bounds
 lambda_p = (1 + sqrt(q))**2
 lambda_m = (1 - sqrt(q))**2
 
+# theoretical Marcenko-Pastur (MP) distribution
 def mp_distribution(x, q, lambda_p, lambda_m):
     # you can also change below to true false statement
     rho = np.sqrt(np.maximum((lambda_p - x)*(x - lambda_m), 0)) / (2 * pi * q * x)
     if q > 1:
-        #rho[np.where(x == 0)] += (q - 1) / q
-        plt.axvline(x=0, linestyle='-', color='sienna', label='_nolegend_')
+        rho *= q
     return rho
 
-x_val = np.linspace(lambda_m, lambda_p + 1, 1000)
-rho_x = mp_distribution(x_val, q, lambda_p, lambda_m)
+# x values for MP distribution
+x = np.linspace(lambda_m, lambda_p + 1, 1000)
+# expected MP distribution of the eigenvalues
+rho = mp_distribution(x, q, lambda_p, lambda_m)
+
 
 # number of zero eigenvalues
-# this is not necessary for T > N 
 nonzero_eig = eigval[eigval > 1e-10]
+# check the number of zero eigenvalues
 num_zero_eig = np.array(np.where(eigval < 1e-10)).size
-print(num_zero_eig)
 
 # marchenko-pastur plot
-plt.plot(x_val, rho_x, 'sienna', label='Theoretical MP Curve')
+plt.plot(x, rho, 'sienna', label='Theoretical MP Curve')
 # bins for eigenvalue distribution
-bins = np.linspace(lambda_m, lambda_p, num=50)
+bins = np.linspace(lambda_m, lambda_p, num=60)
 # histogram of eigenvalues
 plt.hist(nonzero_eig, bins=bins, density=True, color='bisque', label='Eigenvalue Distribution')
 # plot specifications
 plt.xlabel(r'$x$')
 plt.ylabel(r'$\rho_{MP}(x)$')
 plt.title('Marchenko-Pastur (MP) Distribution')
-
+# theoretical MP bounds
 plt.axvline(x=lambda_m, linestyle='--', color='k', label='Theoretical Bound', alpha=0.5, linewidth=0.8)
 plt.axvline(x=lambda_p, linestyle='--', color='k', label='_nolegend_', alpha=0.5, linewidth=0.8)
 
 plt.legend()
-
-print(nonzero_eig)
-
 plt.show()
-
-'''
-FOR N>T NORMALIZE EIGVALS 
-average over many random matrices
-look at arabind'or philipp's papers
-'''
