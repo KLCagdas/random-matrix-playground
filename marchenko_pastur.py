01e1dcb31b736c379803dc10dba09f9f14245d35
Çağdaş Kılıç <kilic.cagdas@metu.edu.tr>
2025-06-04 12:35:07 -0400

    the lower bound for x is changed

--- marchenko_pastur.py
+++ marchenko_pastur.py
@@ -7,16 +7,16 @@ from scipy.signal import unit_impulse
 parameters
 '''
 # number of states
-N = 2000
+N = 1000
 # number of observations
-T = 1000
+T = 2000
 # ratio of num. of states to num. of observations
 q = N/T
 
 # non-symmetric random matrix
 X = np.random.normal(0, 1, (T, N))
 # NxN covariance
-Y = 1/T * X.T @ X
+Y = 1/T * X @ X.T
 # compute the eigenvalues
 eigval = np.linalg.eigvalsh(Y)
 
@@ -32,7 +32,7 @@ def mp_distribution(x, q, lambda_p, lambda_m):
         plt.axvline(x=0, linestyle='-', color='sienna', label='_nolegend_')
     return rho
 
-x_val = np.linspace(0, lambda_p + 1, 1000)
+x_val = np.linspace(lambda_m, lambda_p + 1, 1000)
 rho_x = mp_distribution(x_val, q, lambda_p, lambda_m)
 
 # number of zero eigenvalues
@@ -41,13 +41,20 @@ nonzero_eig = eigval[eigval > 1e-10]
 num_zero_eig = np.array(np.where(eigval < 1e-10)).size
 print(num_zero_eig)
 
-
+# marchenko-pastur plot
 plt.plot(x_val, rho_x, 'sienna', label='Theoretical MP Curve')
-bins = np.linspace(lambda_m, lambda_p, num=40)
+# bins for eigenvalue distribution
+bins = np.linspace(lambda_m, lambda_p, num=50)
+# histogram of eigenvalues
 plt.hist(nonzero_eig, bins=bins, density=True, color='bisque', label='Eigenvalue Distribution')
+# plot specifications
 plt.xlabel(r'$x$')
 plt.ylabel(r'$\rho_{MP}(x)$')
 plt.title('Marchenko-Pastur (MP) Distribution')
+
+plt.axvline(x=lambda_m, linestyle='--', color='k', label='Theoretical Bound', alpha=0.5, linewidth=0.8)
+plt.axvline(x=lambda_p, linestyle='--', color='k', label='_nolegend_', alpha=0.5, linewidth=0.8)
+
 plt.legend()
 
 print(nonzero_eig)
